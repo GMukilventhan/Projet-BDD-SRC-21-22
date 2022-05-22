f94f26bc0e3678c2049d250be4fbb371c3927c45
Mukil <mukil.george@icloud.com>
2022-05-22 17:27:45 +0200

    Catégorie Social

--- IMPORT_DONNEE.sql
+++ IMPORT_DONNEE.sql
@@ -5,3 +5,17 @@ VALUES
     (2,'MONTREUIL'),
     (3,'AULNAY-SOUS-BOIS'),
     (4,'SEVRAN');
+
+--Catégorie_social--
+INSERT INTO categorie_social(ID_CATEGORIE_SOCIAL, CATEGORIE_SOCIAL) 
+VALUES 
+    (1,'Scolaire'),
+    (2,'employé'),
+    (3,'cadre'),
+    (4,'artisant'),
+    (5,'commerçant'),
+    (6,'profession libérale'),
+    (7,'retrainté'),
+    (8,'sans activité');
+
+
