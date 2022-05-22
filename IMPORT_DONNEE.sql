8bd9300cfe972e01878ba311ce8070854420788f
Mukil <mukil.george@icloud.com>
2022-05-22 20:11:45 +0200

    Import donnée + mpd

--- IMPORT_DONNEE.sql
+++ IMPORT_DONNEE.sql
@@ -1,5 +1,5 @@
 --Commune--
-INSERT INTO commune(VILLE) 
+INSERT INTO commune (VILLE) 
 VALUES 
     ('SAINT-DENIS'),
     ('MONTREUIL'),
@@ -7,7 +7,7 @@ VALUES
     ('SEVRAN');
 
 --Catégorie_social--
-INSERT INTO categorie_social(CATEGORIE_SOCIAL) 
+INSERT INTO categorie_social (CATEGORIE_SOCIAL) 
 VALUES 
     ('Scolaire'),
     ('employé'),
@@ -28,9 +28,182 @@ INSERT INTO utilisateur ( NOM, PRENOM, MAIL, ID_COMMUNE, ID_CATEGORIE_SOCIAL)
  ('HABERMANN', 'Maxime', 'mhabermann@mhabermann.fr', 3, 4);
 
 --type_abonnement--
-INSERT INTO type_abonnement(PRIX_PAR_MOIS, NOMBRE_EMPUNT)
+INSERT INTO type_abonnement (PRIX_PAR_MOIS, NOMBRE_EMPUNT)
  VALUES 
     (5.99,10),
     (10.99,20);
 
 
+--Abo--
+INSERT INTO abonnement (DEBUT_ADHESION, FIN_ADHESION, ID_UTILISATEUR, ID_TYPE_ABONNEMENT) 
+VALUES 
+    ('2022-03-05','2022-03-05',1,1),
+    ('2022-03-05','2022-03-05',2,2);
+
+--Artiste--
+INSERT INTO artiste (NOM, PRENOM) 
+VALUES 
+    ('Andrieu','Tarik'),
+    ('Andrieu','Nabil'),
+    ('William','Dmaso'),
+    ('Aurelien','Orelsan'),
+    ('Joseph','Kosinski'),
+    ('Sam','Raimi'),
+    ('Cyril','Hanouna'),
+    ('Philippe','Guillard '),
+    ('Agnès','Hurstel');
+
+--Cassette--
+INSERT INTO casette (TITRE_CASSETTE) 
+VALUES 
+    ('Avengers'),
+    ('Titanic'),
+    ('Star-wars'),
+    ('Sonic'),
+    ('Pokemon');
+
+--disques
+INSERT INTO disques (TITRES_DISQUE) 
+VALUES 
+    ('Deux Frére'),
+    ('Le monde chico'),
+    ('Dans la légende'),
+    ('Que la famille'),
+    ('Ensemble'),
+    ('QALF infinity');
+
+--Music--
+INSERT INTO music (TITRE) 
+VALUES 
+    ('AU DD'),
+    ('Deux Frére'),
+    ('Abonné'),
+    ('jusqu au dernier gramme'),
+    ('DA'),
+    ('ONIZUKA'),
+    ('JE VIS JE VISSER'),
+    ('LALA'),
+    ('MANIFESTE'),
+    ('SHONEN'),
+    ('LIFE LIFE'),
+    ('POUR L ARGENT');
+
+--ecouter--
+INSERT INTO ecouter (ID_DISQUE, ID_MUSIQUE) 
+VALUES
+    ('1','1'),
+    ('1','2'),
+    ('2','3'),
+    ('2','4'),
+    ('3','5'),
+    ('3','6'),
+    ('4','7'),
+    ('4','8'),
+    ('5','9'),
+    ('5','10'),
+    ('6','11'),
+    ('6','12');
+
+--emprunt--
+INSERT INTO  emprunt (DATE_EMPRUNT, ID_ABONNEMENT) 
+VALUES 
+    ('2022-04-05',1),
+    ('2022-05-05',2);
+
+--film--
+INSERT INTO film (TITRE)
+VALUES 
+    ('TOP GUN'),
+    ('DOCTOR STRANGE'),
+    ('LES SEGPA'),
+    ('J ADORE CE QUE VOUS FAITES'),
+    ('LES FOLIES FERMIÈRES'),
+    ('ON SOURIT POUR LA PHOTO');
+
+--GENRE--
+INSERT INTO genre (GENRE) 
+VALUES
+    ('AVENTURE'),
+    ('COMÉDIE'),
+    ('POLICIER'),
+    ('SCIENCE FICTION');
+
+--penalite--
+INSERT INTO penalite (TYPE_OBJET, PRIX_J_PENALITER) 
+VALUES
+    ('CASSETTE',3.99),
+    ('DISQUE',5.99);
+
+--categoriser--
+INSERT INTO categoriser (ID_FILM, ID_GENRE) 
+VALUES 
+    ('1','3'),
+    ('2','1'),
+    ('3','2'),
+    ('4','2'),
+    ('5','1'),
+    ('6','2');
+
+--composer--
+INSERT INTO composer (ID_MUSIQUE, ID_ARTISTE)
+VALUES
+    ('1','1'),
+    ('2','2'),
+    ('3','2'),
+    ('4','1'),
+    ('5','1'),
+    ('6','2'),
+    ('7','2'),
+    ('8','1'),
+    ('9','4'),
+    ('10','4'),
+    ('11','3'),
+    ('12','3');
+
+--concevoir--
+INSERT INTO concevoir (ID_FILM, ID_ARTISTE)
+VALUES
+    ('1','5'),
+    ('2','6'),
+    ('3','7'),
+    ('4','8'),
+    ('5','9'),
+    ('6','7');
+
+
+--interpreter--
+INSERT INTO INTERPRETER (ID_MUSIQUE, ID_ARTISTE)
+VALUES
+    ('1','6'),
+    ('2','7'),
+    ('3','6'),
+    ('4','7'),
+    ('5','6'),
+    ('6','7'),
+    ('7','7'),
+    ('8','6'),
+    ('9','9'),
+    ('10','9'),
+    ('11','8'),
+    ('12','8');
+
+--jouer--
+INSERT INTO jouer (ID_FILM, ID_ARTISTE)
+VALUES
+    ('1','5'),
+    ('2','6'),
+    ('3','7'),
+    ('4','8'),
+    ('5','9'),
+    ('6','7');
+
+--objet--
+INSERT INTO objet (NOMBRE_ARTICLE, PRIX_OBJET, ID_PENALITE) 
+VALUES 
+    ([value-2],[value-3],[value-4])
+--emprunter--
+--produire--
+--rendre--
+--retour--
+--retourner--
+--visioner--
